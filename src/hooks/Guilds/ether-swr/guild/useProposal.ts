2139920c75e4d5d38e797548fd89109554989506
Dino <crescimbenidino@gmail.com>
2022-05-28 18:51:21 -0300

    feat(useProposal): added cases in middleware to map state numbers to state descriptions (active, passed, executed, failed)

--- src/hooks/Guilds/ether-swr/guild/useProposal.ts
+++ src/hooks/Guilds/ether-swr/guild/useProposal.ts
@@ -3,14 +3,30 @@ import { Middleware, SWRHook } from 'swr';
 import { Proposal } from '../../../../types/types.guilds';
 import useEtherSWR from '../useEtherSWR';
 import ERC20GuildContract from 'contracts/ERC20Guild.json';
+import { ProposalState } from 'Components/Types';
 
 const formatterMiddleware: Middleware =
   (useSWRNext: SWRHook) => (key, fetcher, config) => {
     const swr = useSWRNext(key, fetcher, config);
     if (swr.data) {
       const original = swr.data as any;
-
       const clone: any = Object.assign({}, swr.data);
+
+      switch (clone.state) {
+        case 1:
+          clone.state = [ProposalState.Active];
+          break;
+        case 2:
+          clone.state = [ProposalState.Passed];
+          break;
+        case 3:
+          clone.state = [ProposalState.Executed];
+          break;
+        case 4:
+          clone.state = [ProposalState.Failed];
+          break;
+      }
+
       clone.startTime = original.startTime
         ? unix(original.startTime.toNumber())
         : null;
@@ -24,11 +40,12 @@ const formatterMiddleware: Middleware =
   };
 
 export const useProposal = (guildId: string, proposalId: string) => {
-  return useEtherSWR<Proposal>(
+  let result = useEtherSWR<Proposal>(
     guildId && proposalId ? [guildId, 'getProposal', proposalId] : [],
     {
       use: [formatterMiddleware],
       ABIs: new Map([[guildId, ERC20GuildContract.abi]]),
     }
   );
+  return result;
 };
