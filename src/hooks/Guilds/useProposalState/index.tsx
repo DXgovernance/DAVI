9696e2cb0c6f318ff4141ef738c4a1de742ee9f3
Dino <crescimbenidino@gmail.com>
2022-05-30 13:16:48 -0300

    refactor(useProposalState): deleted unused useMemo

--- src/hooks/Guilds/useProposalState/index.tsx
+++ src/hooks/Guilds/useProposalState/index.tsx
@@ -1,30 +1,25 @@
 import moment from 'moment';
-import { useMemo } from 'react';
 import { Proposal, ContractState } from 'types/types.guilds.d';
 
 export const useProposalState = (proposal: Proposal): ContractState => {
-  const status = useMemo(() => {
-    if (!proposal?.endTime) return null;
-    switch (proposal.contractState) {
-      case ContractState.Active:
-        const currentTime = moment();
-        if (currentTime.isSameOrAfter(proposal.endTime)) {
-          return ContractState.Failed;
-        } else {
-          return ContractState.Active;
-        }
-      case ContractState.Executed:
-        return ContractState.Executed;
-      case ContractState.Rejected:
-        return ContractState.Rejected;
-      case ContractState.Failed:
+  if (!proposal?.endTime) return null;
+  switch (proposal.contractState) {
+    case ContractState.Active:
+      const currentTime = moment();
+      if (currentTime.isSameOrAfter(proposal.endTime)) {
         return ContractState.Failed;
-      default:
-        return null;
-    }
-  }, [proposal]);
-
-  return status;
+      } else {
+        return ContractState.Active;
+      }
+    case ContractState.Executed:
+      return ContractState.Executed;
+    case ContractState.Rejected:
+      return ContractState.Rejected;
+    case ContractState.Failed:
+      return ContractState.Failed;
+    default:
+      return null;
+  }
 };
 
 export default useProposalState;
