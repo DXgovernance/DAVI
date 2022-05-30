2b0f25f8f80c1492f7231b65580fb4e01da0f42c
Milton <milton.tulli@gmail.com>
2022-05-30 13:15:31 -0300

    refactor proposalTypes component, add test, stories, include translation setup in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GuildsDarkTheme } from 'Components/theme';
+import initializeI18Next from '../src/i18n';
 import GlobalStyle from 'theme/GlobalTheme';
 import MultichainProvider from 'contexts/MultichainProvider/index';
+
+initializeI18Next();
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
