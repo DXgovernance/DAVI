09aa75a1001b13080e265ac37728511f6acd9a9e
Dino <crescimbenidino@gmail.com>
2022-05-31 14:41:01 -0300

    feat(ExecuteButton): added i18n to ExecuteButton component

--- src/Components/ExecuteButton/index.tsx
+++ src/Components/ExecuteButton/index.tsx
@@ -1,14 +1,17 @@
 import { Button } from 'old-components/Guilds/common/Button';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface ExecuteButtonProps {
   executeProposal: () => void;
 }
 
 const ExecuteButton: React.FC<ExecuteButtonProps> = ({ executeProposal }) => {
+  const { t } = useTranslation();
+
   return (
     <Button data-testid="execute-btn" onClick={() => executeProposal()}>
-      Execute
+      {t('execute')}
     </Button>
   );
 };
