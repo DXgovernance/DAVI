411227350e7990f4a2fa772dd087ad30e09a1e66
Dino <crescimbenidino@gmail.com>
2022-06-02 08:52:19 -0300

    refactor: created new ProposalInfoCard component

---
+++ src/Components/ProposalInfoCard/ProposalHistory/ProposalHistory.tsx
@@ -0,0 +1,40 @@
+import { Loading } from 'Components/Primitives/Loading';
+import moment from 'moment';
+import { Divider } from 'old-components/Guilds/common/Divider';
+
+import { useMemo } from 'react';
+import { InfoItem } from '../InfoItem';
+import { ProposalHistoryProps } from './ProposalHistory.types';
+
+const ProposalHistory: React.FC<ProposalHistoryProps> = ({ proposal }) => {
+  const endDetail = useMemo(() => {
+    if (!proposal || !proposal.endTime) return null;
+
+    const currentTime = moment();
+    let prefix = proposal.endTime.isBefore(currentTime) ? 'Ended' : 'Ends';
+    return `${prefix} ${proposal.endTime.fromNow()}`;
+  }, [proposal]);
+
+  return (
+    <>
+      <Divider />
+
+      {!proposal ? (
+        <Loading loading text skeletonProps={{ height: '100px' }} />
+      ) : (
+        <>
+          <InfoItem
+            title="Proposal created"
+            description={proposal.startTime.format('MMM Do, YYYY - h:mm a')}
+          />
+          <InfoItem
+            title={endDetail}
+            description={proposal.endTime.format('MMM Do, YYYY - h:mm a')}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default ProposalHistory;
