6e4ab5f47b43afee5afb3f89453806179d95ce65
Kenny Chung <kenny.chung@mindler.se>
2022-06-10 23:14:42 +0200

    fix: add jest mocks

--- src/Components/StakeTokensModal/StakeTokensModal.test.tsx
+++ src/Components/StakeTokensModal/StakeTokensModal.test.tsx
@@ -2,6 +2,11 @@ import StakeTokensModal from './StakeTokensModal';
 import { render } from '../../utils/tests';
 import { mockStakeTokensModalProps } from './fixture';
 
+jest.mock('ipfs', () => jest.fn());
+jest.mock('cids', () => jest.fn());
+jest.mock('orbit-db', () => jest.fn());
+jest.mock('ipfs-only-hash', () => jest.fn());
+
 describe('StakeTokensModal', () => {
   it('StakeTokensModal renders properly', () => {
     const { container } = render(
