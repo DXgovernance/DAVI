a6f4f7c5dd75c40db591c1453136786f3753ea96
Dino <crescimbenidino@gmail.com>
2022-06-13 16:25:40 -0300

    feat(SwaprPicker): replaced dummy data with fetch from the graph

--- src/Components/SwaprPicker/SwaprPicker.tsx
+++ src/Components/SwaprPicker/SwaprPicker.tsx
@@ -1,29 +1,65 @@
 import { Picker } from 'Components/Primitives/Forms/Picker';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { dummyData } from './dummyData';
 import { TransparentButton } from './SwaprPicker.styled';
 import { SwaprPickerProps } from './types';
 import AddressButton from 'Components/AddressButton/AddressButton';
+import { useContext } from '../../contexts';
+import { Loading } from 'Components/Primitives/Loading';
+import moment from 'moment';
 
 const SwaprPicker: React.FC<SwaprPickerProps> = ({ value, onChange }) => {
   const { t } = useTranslation();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  let data = dummyData.pairs;
+  const {
+    context: { subgraphService },
+  } = useContext();
+  const [swaprPairsFetchedData, setSwaprPairsFetchedData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getSwaprPairData = async () => {
+    const currentUnixTimestamp = moment().unix();
+    const pageSize = 1000;
+    const data = await subgraphService.getSwaprPairs(
+      currentUnixTimestamp,
+      '',
+      pageSize,
+      ''
+    );
+    setSwaprPairsFetchedData(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getSwaprPairData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const parsedPairs = useMemo(() => {
-    return data.map(pair => {
+    return swaprPairsFetchedData?.map(pair => {
       return {
         ...pair,
-        title: `${pair?.token0.name}-${pair?.token1.name}`,
+        title: `${pair?.token0.symbol} - ${pair?.token1.symbol}`,
+        subtitle: `${pair?.token0.name} - ${pair?.token1.name}`,
       };
     });
-  }, [data]);
+  }, [swaprPairsFetchedData]);
 
   const onSelect = value => {
     onChange(value.address);
     setIsModalOpen(false);
   };
+
+  if (isLoading)
+    return (
+      <TransparentButton
+        variant="secondary"
+        aria-label="Skeleton loading button"
+        type="button"
+      >
+        <Loading loading text />
+      </TransparentButton>
+    );
 
   return (
     <>
