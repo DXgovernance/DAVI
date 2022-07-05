e52df1289f22f46bf3b1e671cad43ba3d99306da
Dino <crescimbenidino@gmail.com>
2022-07-05 11:08:05 -0300

    fix(DurationInput): component can be passed a value and parses it to determine duration

--- src/hooks/Guilds/useDuration/index.ts
+++ src/hooks/Guilds/useDuration/index.ts
@@ -1,8 +1,9 @@
-import moment, { Duration, DurationInputArg2 } from 'moment';
 import { useMemo, useState } from 'react';
+import { DURATION_IN_SECONDS } from 'constants/Duration';
 
-export const useDuration = () => {
-  const [duration, setDuration] = useState({
+export const useDuration = (passedValue: number) => {
+  const [durationString, setDurationString] = useState('');
+  const [durationObject, setDurationObject] = useState({
     years: 0,
     months: 0,
     days: 0,
@@ -11,15 +12,74 @@ export const useDuration = () => {
     seconds: 0,
   });
 
+  useMemo(() => {
+    let durationObjectCopy = { ...durationObject };
+    let durationObjectKeys = Object.keys(durationObjectCopy);
+    let numberOfSeconds = passedValue;
+
+    durationObjectKeys.reduce((previous, current) => {
+      let numberOfUnit = Math.floor(previous / DURATION_IN_SECONDS[current]);
+      durationObjectCopy[current] = numberOfUnit;
+      previous -= numberOfUnit * DURATION_IN_SECONDS[current];
+      return previous;
+    }, numberOfSeconds);
+
+    setDurationObject(durationObjectCopy);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [passedValue]);
+
+  useMemo(() => {
+    let numbersToStringDuration = Object.keys(durationObject).reduce(
+      (durationString, timePeriod) => {
+        if (durationObject[timePeriod] === 0) return durationString;
+
+        let formattedTimePeriod =
+          durationObject[timePeriod] === 1
+            ? `${timePeriod.slice(0, -1)}`
+            : `${timePeriod}`;
+
+        let separator = durationString.length === 0 ? '' : ', ';
+
+        return (
+          durationString +
+          `${separator}${durationObject[timePeriod]} ${formattedTimePeriod}`
+        );
+      },
+      ''
+    );
+
+    const replaceLastCommaWithAnd = (string: string) => {
+      const lastIndex = string.lastIndexOf(',');
+      if (lastIndex === -1) return string;
+
+      const replacedString =
+        string.substring(0, lastIndex) +
+        ' and' +
+        string.substring(lastIndex + 1);
+      return replacedString;
+    };
+
+    numbersToStringDuration = replaceLastCommaWithAnd(numbersToStringDuration);
+
+    setDurationString(numbersToStringDuration);
+  }, [durationObject]);
+
   const handleConversion = (value: string) => {
     if (!value) return 0;
     return parseInt(value);
   };
 
   const increment = (key: string) =>
-    setDuration({ ...duration, [key]: handleConversion(duration[key]) + 1 });
+    setDurationObject({
+      ...durationObject,
+      [key]: handleConversion(durationObject[key]) + 1,
+    });
   const decrement = (key: string) =>
-    setDuration({ ...duration, [key]: handleConversion(duration[key]) - 1 });
+    setDurationObject({
+      ...durationObject,
+      [key]: handleConversion(durationObject[key]) - 1,
+    });
 
   // convert to number form string and then calculate
   const handleChange = (e: string, value: string) => {
@@ -27,33 +87,15 @@ export const useDuration = () => {
     if (e === '') formattedValue = 0;
     else formattedValue = parseInt(e);
 
-    return setDuration({ ...duration, [value]: formattedValue });
+    return setDurationObject({ ...durationObject, [value]: formattedValue });
   };
 
-  const { time } = useMemo(() => {
-    const convertDurationToSeconds = Object.keys(duration).reduce(
-      (acc, curr) => {
-        const result = acc.add(
-          moment.duration(duration[curr], curr as DurationInputArg2)
-        );
-        return result;
-      },
-      moment.duration(0, 'years') as Duration
-    );
-    return {
-      time: convertDurationToSeconds.asSeconds(),
-    };
-  }, [duration]);
-
   let result = {
-    data: {
-      duration,
-      setDuration,
-      time,
-      handleChange,
-      increment,
-      decrement,
-    },
+    durationObject,
+    durationString,
+    handleChange,
+    increment,
+    decrement,
   };
 
   return result;
