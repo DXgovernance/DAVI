a201b0e7f5f2e2acc610bc5b4dc73877c8d9ad49
Dino <crescimbenidino@gmail.com>
2022-07-05 15:30:09 -0300

    feat: decrease button disables if you cant reduce that duration

--- src/Components/Primitives/Forms/DurationInput/DurationInput.tsx
+++ src/Components/Primitives/Forms/DurationInput/DurationInput.tsx
@@ -13,6 +13,7 @@ import {
 } from './DurationInput.styled';
 import { DurationInputProps } from './types';
 import { useTranslation } from 'react-i18next';
+import { DURATION_IN_SECONDS } from 'constants/Duration';
 
 const DurationInput: React.FC<DurationInputProps> = ({ value, onChange }) => {
   const { durationObject, durationString, handleChange, increment, decrement } =
@@ -20,6 +21,11 @@ const DurationInput: React.FC<DurationInputProps> = ({ value, onChange }) => {
 
   const [isOpen, setIsOpen] = useState(false);
   const { t } = useTranslation();
+
+  const checkDisabledButton = (duration: string) => {
+    if (value < DURATION_IN_SECONDS[duration]) return true;
+    else return false;
+  };
 
   return (
     <>
@@ -65,6 +71,7 @@ const DurationInput: React.FC<DurationInputProps> = ({ value, onChange }) => {
                     onClick={() => decrement(duration)}
                     data-testid={`lower-limit-btn-${duration}`}
                     aria-label={`Decrease ${duration}`}
+                    disabled={checkDisabledButton(duration)}
                   >
                     <GoTriangleDown />
                   </ColumnButton>
