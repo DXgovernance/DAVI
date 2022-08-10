5ec033eff7391b28f25ef9353c932a315b6cd473
Kenny <59825171+Kenny-Gin1@users.noreply.github.com>
2022-08-10 13:35:13 +0200

    fix: fix broken storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
   ],
   "framework": "@storybook/react"
 }
