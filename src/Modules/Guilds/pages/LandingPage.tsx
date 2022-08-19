695bef3170c80ec77ccbd6c77f2440bdd3c4a8dd
rossneilson <rossneilson99@gmail.com>
2022-08-19 16:10:51 +0100

    feat: Remove landing page filter and create button

--- src/Modules/Guilds/pages/LandingPage.tsx
+++ src/Modules/Guilds/pages/LandingPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+// import { AiOutlineSearch } from 'react-icons/ai';
+// import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { useGuildRegistry } from 'hooks/Guilds/ether-swr/guild/useGuildRegistry';
 import GuildCard from 'Components/GuildCard/GuildCard';
-import { Button } from 'old-components/Guilds/common/Button';
-import Input from 'old-components/Guilds/common/Form/Input';
+// import { Button } from 'old-components/Guilds/common/Button';
+// import Input from 'old-components/Guilds/common/Form/Input';
 import { Flex } from 'Components/Primitives/Layout';
 
 import useGuildMemberTotal from 'hooks/Guilds/ether-swr/guild/useGuildMemberTotal';
@@ -14,22 +14,22 @@ import useActiveProposalsNow from 'hooks/Guilds/ether-swr/guild/useGuildActivePr
 import useENSNameFromAddress from 'hooks/Guilds/ether-swr/ens/useENSNameFromAddress';
 import { useGuildConfig } from 'hooks/Guilds/ether-swr/guild/useGuildConfig';
 
-const InputContainer = styled(Flex)`
-  flex-direction: row;
-  /* Medium devices (landscape tablets, 768px and up) */
-  @media only screen and (min-width: 768px) {
-    grid-template-columns: 300px minmax(0, 1fr);
-  }
-`;
+// const InputContainer = styled(Flex)`
+//   flex-direction: row;
+//   /* Medium devices (landscape tablets, 768px and up) */
+//   @media only screen and (min-width: 768px) {
+//     grid-template-columns: 300px minmax(0, 1fr);
+//   }
+// `;
 
-const StyledButton = styled(Button).attrs(() => ({
-  variant: 'secondary',
-}))`
-  margin-left: 1rem;
-  width: 9rem;
-  padding: 0.7rem;
-  color: ${({ theme }) => theme.colors.text};
-`;
+// const StyledButton = styled(Button).attrs(() => ({
+//   variant: 'secondary',
+// }))`
+//   margin-left: 1rem;
+//   width: 9rem;
+//   padding: 0.7rem;
+//   color: ${({ theme }) => theme.colors.text};
+// `;
 
 const CardsContainer = styled(Flex)`
   display: flex;
@@ -40,17 +40,17 @@ const CardsContainer = styled(Flex)`
   gap: 1.7rem;
 `;
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: ${({ theme }) => theme.colors.text};
-  &:focus,
-  &:hover,
-  &:visited,
-  &:link,
-  &:active {
-    text-decoration: none;
-  }
-`;
+// const StyledLink = styled(Link)`
+//   text-decoration: none;
+//   color: ${({ theme }) => theme.colors.text};
+//   &:focus,
+//   &:hover,
+//   &:visited,
+//   &:link,
+//   &:active {
+//     text-decoration: none;
+//   }
+// `;
 
 const GuildCardLoader = () => {
   return (
@@ -92,7 +92,7 @@ const LandingPage: React.FC = () => {
 
   return (
     <>
-      <InputContainer>
+      {/* <InputContainer>
         <Input
           value=""
           icon={<AiOutlineSearch size={24} />}
@@ -104,7 +104,7 @@ const LandingPage: React.FC = () => {
             {t('guilds.create')}
           </StyledLink>
         </StyledButton>
-      </InputContainer>
+      </InputContainer> */}
       <CardsContainer>
         {error ? (
           <>{/* Render error state */}</>
