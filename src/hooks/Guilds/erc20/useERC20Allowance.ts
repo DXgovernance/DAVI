449291506f5d8fa1b3ce51cc837680c797a5ba7f
Kenny <kenny.chung@live.se>
2022-08-26 14:00:50 +0200

    refactor: fixed every erc20 hook to wagmi

--- src/hooks/Guilds/erc20/useERC20Allowance.ts
+++ src/hooks/Guilds/erc20/useERC20Allowance.ts
@@ -1,53 +1,21 @@
-import useEtherSWR from '../ether-swr/useEtherSWR';
 import ERC20ABI from 'abis/ERC20.json';
-import { useMemo } from 'react';
 import { BigNumber } from 'ethers';
+import { useContractRead } from 'wagmi';
 
 export const useERC20Allowance = (
   contractAddress: string,
   walletAddress: string,
   spenderAddress: string
 ) => {
-  console.log({ contractAddress, walletAddress, spenderAddress });
-  const { data, ...rest } = useEtherSWR(
-    contractAddress && walletAddress
-      ? [contractAddress, 'allowance', walletAddress, spenderAddress]
-      : [],
-    {
-      ABIs: new Map([[contractAddress, ERC20ABI]]),
-    }
-  );
-
-  const parsed = useMemo(() => {
-    if (!data) return undefined;
-
-    return BigNumber.from(data);
-  }, [data]);
-
-  return { data: parsed, ...rest };
+  const { data, isError, isLoading } = useContractRead({
+    addressOrName: contractAddress,
+    contractInterface: ERC20ABI,
+    functionName: 'allowance',
+    args: [walletAddress, spenderAddress],
+  });
+  return {
+    data: data ? BigNumber.from(data) : undefined,
+    isError,
+    isLoading,
+  };
 };
-
-// import ERC20ABI from 'abis/ERC20.json';
-// import { BigNumber } from 'ethers';
-// import { useContractRead } from 'wagmi';
-
-// export const useERC20Allowance = (
-//   contractAddress: string,
-//   walletAddress: string,
-//   spenderAddress: string
-// ) => {
-//   console.log({ contractAddress, walletAddress, spenderAddress });
-//   const { data, isError, isLoading } = useContractRead({
-//     addressOrName: contractAddress,
-//     contractInterface: ERC20ABI,
-//     functionName: 'allowance',
-//     args: [walletAddress, spenderAddress],
-//     suspense: true
-//   });
-
-//   return {
-//     data: BigNumber.from(data),
-//     isError,
-//     isLoading,
-//   };
-// };
