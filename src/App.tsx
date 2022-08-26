abc735b9f7305c57b7f897679cb0e425520f920e
Dino <crescimbenidino@gmail.com>
2022-08-26 10:29:36 -0300

    fix: navigation

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,12 @@ import { ProposalTypes } from 'components/ProposalTypes';
 import CreateProposalPage from 'Modules/Guilds/pages/CreateProposal';
 import { LandingPage } from 'Modules/Guilds/pages/LandingPage';
 import NotFound from 'Modules/Guilds/pages/NotFound';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GuildsDarkTheme } from 'components/theme';
 import CreateDiscussionPage from 'Modules/Guilds/pages/CreateDiscussion';
-import { useNetwork } from 'wagmi';
 
 const App = () => {
-  const {
-    chain: { network },
-  } = useNetwork();
-
   return (
     <ThemeProvider theme={GuildsDarkTheme}>
       <TransactionsProvider>
@@ -29,7 +24,6 @@ const App = () => {
           <Header />
           <Container>
             <Routes>
-              <Route path="/" element={<Navigate replace to={network} />} />
               <Route path="/:chainName" element={<LandingPage />} />
               <Route path="/:chainName/:guildId" element={<GuildsPage />} />
               <Route
