f923faa7f1944c9209b48ebe11b13ffbc2f987bb
Dino <crescimbenidino@gmail.com>
2022-08-26 10:32:23 -0300

    feat: added translations

--- src/Modules/Guilds/pages/NotFound.tsx
+++ src/Modules/Guilds/pages/NotFound.tsx
@@ -2,17 +2,22 @@ import { IconButton } from 'components/primitives/Button';
 import { Result, ResultState } from 'components/Result';
 import { UnstyledLink } from 'components/primitives/Links';
 import { FiArrowLeft } from 'react-icons/fi';
+import { useTypedParams } from '../Hooks/useTypedParams';
+import { useTranslation } from 'react-i18next';
 
 const NotFound: React.FC = () => {
+  const { chainName } = useTypedParams();
+  const { t } = useTranslation();
+
   return (
     <Result
       state={ResultState.ERROR}
-      title="That page doesn't exist."
-      subtitle="Make sure you typed the correct address."
+      title={t('pageNotExist')}
+      subtitle={t('makeSureCorrectAddress')}
       extra={
-        <UnstyledLink to={`/`}>
+        <UnstyledLink to={`/${chainName}`}>
           <IconButton iconLeft>
-            <FiArrowLeft /> Take me home
+            <FiArrowLeft /> {t('takeMeHome')}
           </IconButton>
         </UnstyledLink>
       }
