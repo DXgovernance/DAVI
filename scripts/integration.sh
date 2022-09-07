57e57ddb2d9879ee873ad116dd7e6f9a2191446e
kacper29 <niemam29@gmail.com>
2022-09-07 14:35:34 +0200

    Added dashboard support

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Get record key from env
-# export $(grep -v '^#' .env | xargs)
-# RECORD_KEY="${CYPRESS_RECORD_KEY}"
+export $(grep -v '^#' .env | xargs)
+RECORD_KEY="${CYPRESS_RECORD_KEY}"
 
 export SECRET_WORDS="cream core pear sure dinner indoor citizen divorce sudden captain subject remember"
 export PASSWORD="TestMetaMask"
@@ -16,5 +16,4 @@ export FAIL_ON_ERROR=0
 # export SKIP_METAMASK_INSTALL=false
 # export SKIP_METAMASK_SETUP=false
 
-synpress run --configFile ./cypress/config/development.json
-# TODO: Include monitoring  with record key like: synpress run --configFile synpress.json --record --key $RECORD_KEY
+synpress run --configFile ./cypress/config/development.json --record --key $RECORD_KEY
