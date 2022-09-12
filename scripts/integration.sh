5e879de9534ec1b156717d2494e570360aadb4f9
kacper29 <niemam29@gmail.com>
2022-09-12 13:01:08 +0200

    Added pipe

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -12,4 +12,8 @@ export FAIL_ON_ERROR=0
 # export SKIP_METAMASK_INSTALL=false
 # export SKIP_METAMASK_SETUP=false
 
-synpress run --configFile ./cypress/config/development.json --record --key $CYPRESS_RECORD_KEY
+if [[ $CYPRESS_RECORD_KEY ]]; then
+  synpress run --configFile ./cypress/config/development.json --record --key $CYPRESS_RECORD_KEY
+else
+  synpress run --configFile ./cypress/config/development.json
+fi
