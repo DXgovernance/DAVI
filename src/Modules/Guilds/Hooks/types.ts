c1433415f5cefacf851efe78577fff92cd49a4e1
Milton Tulli <milton.tulli@gmail.com>
2022-09-12 13:24:33 -0300

    Skip lock tokens e2e test, userVote fixes

--- src/Modules/Guilds/Hooks/types.ts
+++ src/Modules/Guilds/Hooks/types.ts
@@ -1,32 +1,14 @@
-export type WagmiUseContractReadResponse<T> = Pick<
-  import('react-query').QueryObserverResult<
-    import('ethers/lib/utils').Result,
-    Error
-  >,
-  | 'error'
-  | 'fetchStatus'
-  | 'isError'
-  | 'isFetched'
-  | 'isFetching'
-  | 'isLoading'
-  | 'isRefetching'
-  | 'isSuccess'
-  | 'refetch'
-> & {
-  isIdle: boolean;
-  status: 'error' | 'success' | 'idle' | 'loading' /** Subscribe to changes */;
-  internal: Pick<
-    import('react-query').QueryObserverResult<unknown, unknown>,
-    | 'dataUpdatedAt'
-    | 'errorUpdatedAt'
-    | 'failureCount'
-    | 'isFetchedAfterMount'
-    | 'isLoadingError'
-    | 'isPaused'
-    | 'isPlaceholderData'
-    | 'isPreviousData'
-    | 'isRefetchError'
-    | 'isStale'
-    | 'remove'
-  >;
-} & { data: T };
+// TODO: Wagmi current version (0.5.6) does not have types for useContractRead hook. Use wagmi type in when is ready and remove this.
+export type WagmiUseContractReadResponse<T> = {
+  data: T;
+  error: any;
+  isError: boolean;
+  isFetched: boolean;
+  isFetching: boolean;
+  isLoading: boolean;
+  isRefetching: boolean;
+  isSuccess: boolean;
+  refetch: (options?: any) => Promise<any>;
+  status: 'error' | 'success' | 'idle' | 'loading';
+  fetchStatus: 'fetching' | 'paused' | 'idle';
+};
--- src/components/ProposalVoteCard/components/UserVote/UserVote.tsx
+++ src/components/ProposalVoteCard/components/UserVote/UserVote.tsx
@@ -1,23 +1,9 @@
-import useVotingPowerPercent from 'Modules/Guilds/Hooks/useVotingPowerPercent';
+import { useTranslation } from 'react-i18next';
 import { formatUnits } from 'ethers/lib/utils';
-import type { VoteData } from 'Modules/Guilds/Hooks/useVotingResults';
-import { Box } from 'components/primitives/Layout/Box';
-import styled from 'styled-components';
-import { UseProposalVotesOfVoterReturn } from 'Modules/Guilds/Hooks/useProposalVotesOfVoter';
 
-const Container = styled(Box)`
-  margin: 1rem 0 0;
-  border-radius: 0.375rem;
-  padding: 8px;
-  font-size: ${({ theme }) => theme.fontSizes.label};
-  background: ${({ theme }) => theme.colors.border1};
-`;
-interface UserVoteProps {
-  votedOptionLabel: string;
-  voteData: VoteData;
-  isPercent: boolean;
-  userVote: UseProposalVotesOfVoterReturn;
-}
+import useVotingPowerPercent from 'Modules/Guilds/Hooks/useVotingPowerPercent';
+import { Container } from './UserVote.styled';
+import { UserVoteProps } from './types';
 
 const UserVote: React.FC<UserVoteProps> = ({
   voteData,
@@ -25,19 +11,20 @@ const UserVote: React.FC<UserVoteProps> = ({
   isPercent,
   userVote,
 }) => {
+  const { t } = useTranslation();
   const votingPowerPercent = useVotingPowerPercent(
-    userVote.votingPower,
+    userVote?.votingPower,
     voteData?.totalLocked,
     2
   );
 
-  const voting = `${formatUnits(userVote.votingPower || 0)} ${
+  const voting = `${formatUnits(userVote?.votingPower || 0)} ${
     voteData?.token?.symbol
   }`;
 
   return userVote.action ? (
     <Container>
-      You voted for option "{votedOptionLabel}" with{' '}
+      {t('youVotedForOption', { optionLabel: votedOptionLabel })} {t('with')}{' '}
       {isPercent ? `${votingPowerPercent}%` : voting}
     </Container>
   ) : null;
