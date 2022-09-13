36383596d83fd235a7c7a5c8afd77f66ec20ec69
Dino <crescimbenidino@gmail.com>
2022-09-13 10:34:06 -0300

    fix: update ENS action not showing on proposal card

--- src/hooks/Guilds/contracts/useDecodedCall.ts
+++ src/hooks/Guilds/contracts/useDecodedCall.ts
@@ -8,6 +8,7 @@ import {
 import ERC20 from 'contracts/ERC20.json';
 import ERC20SnapshotRep from 'contracts/ERC20SnapshotRep.json';
 import PermissionRegistry from 'contracts/PermissionRegistry.json';
+import ENSPublicResolver from 'contracts/ENSPublicResolver.json';
 import {
   ApproveSendTokens,
   Call,
@@ -20,6 +21,7 @@ import {
   ERC20_TRANSFER_SIGNATURE,
   SET_PERMISSION_SIGNATURE,
   MINT_REP_SIGNATURE,
+  ENS_UPDATE_CONTENT,
 } from 'utils';
 import { lookUpContractWithSourcify } from 'utils/sourcify';
 
@@ -40,6 +42,10 @@ const knownSigHashes: Record<string, { callType: SupportedAction; ABI: any }> =
     [MINT_REP_SIGNATURE]: {
       callType: SupportedAction.REP_MINT,
       ABI: ERC20SnapshotRep.abi,
+    },
+    [ENS_UPDATE_CONTENT]: {
+      callType: SupportedAction.ENS_UPDATE_CONTENT,
+      ABI: ENSPublicResolver.abi,
     },
   };
 
