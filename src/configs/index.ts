884c0e13085084337c9b68d9fed5bfeae70ccba0
rossneilson <rossneilson99@gmail.com>
2022-10-04 09:47:25 -0500

    fix: Adding mainnet and gc to voting contract data

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,1 +1,1 @@
-export const RICH_CONTRACT_DATA_REGISTRY = 'QmW8zpYQ3tMtcVBGhaGGxcUE8euaerjVXErBumj95TErcY';
+export const RICH_CONTRACT_DATA_REGISTRY = 'QmPuywuaNbfVoTCm12J6UgkcJVkEjjEh9k6y9t8gakCbJj';
