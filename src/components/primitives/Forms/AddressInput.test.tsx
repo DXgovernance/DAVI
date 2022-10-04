f572eb014947101cab6a16ab77969ecfd6479099
Dino <crescimbenidino@gmail.com>
2022-10-04 08:00:54 -0300

    test(AddressInput.test.tsx): added tests

--- src/components/primitives/Forms/AddressInput.test.tsx
+++ src/components/primitives/Forms/AddressInput.test.tsx
@@ -1,5 +1,6 @@
 import { fireEvent } from '@testing-library/react';
 import { MOCK_ADDRESS, MOCK_ENS_NAME } from 'hooks/Guilds/ens/fixtures';
+import { ZERO_ADDRESS } from 'utils';
 import { render } from 'utils/tests';
 import { AddressInput } from './AddressInput';
 
@@ -30,6 +31,82 @@ jest.mock('hooks/Guilds/ens/useENSAvatar', () => ({
 }));
 
 describe(`AddressInput`, () => {
+  describe(`General interaction`, () => {
+    it(`should clear the input if the X is clicked`, async () => {
+      const { findByTestId } = render(
+        <AddressInput value={MOCK_ADDRESS} onChange={jest.fn()} />
+      );
+
+      const addressField = await findByTestId('address input');
+      const clearAddressButton = await findByTestId('clear address');
+      fireEvent.click(clearAddressButton);
+
+      expect(addressField).toHaveDisplayValue('');
+    });
+
+    it(`should be disabled`, async () => {
+      const { findByTestId } = render(
+        <AddressInput
+          value={MOCK_ADDRESS}
+          onChange={jest.fn()}
+          disabled={true}
+        />
+      );
+
+      const addressField = await findByTestId('address input');
+
+      expect(addressField).toBeDisabled();
+    });
+
+    it(`should show the avatar if the address is valid`, async () => {
+      const { findByTestId } = render(
+        <AddressInput
+          value={MOCK_ADDRESS}
+          onChange={jest.fn()}
+          enableENSName={false}
+        />
+      );
+
+      const addressAvatar = await findByTestId('avatar');
+
+      expect(addressAvatar).toBeInTheDocument();
+    });
+
+    it(`should show the avatar if the ENS name is valid`, async () => {
+      const { findByTestId } = render(
+        <AddressInput value={MOCK_ADDRESS} onChange={jest.fn()} />
+      );
+
+      const addressAvatar = await findByTestId('avatar');
+
+      expect(addressAvatar).toBeInTheDocument();
+    });
+
+    it(`shouldn't show the avatar if the address is invalid`, async () => {
+      const { queryByTestId } = render(
+        <AddressInput
+          value={'0xinvalidAddress'}
+          onChange={jest.fn()}
+          enableENSName={false}
+        />
+      );
+
+      const addressAvatar = queryByTestId('avatar');
+
+      expect(addressAvatar).not.toBeInTheDocument();
+    });
+
+    it(`shouldn't show the avatar if the ENS name is invalid`, async () => {
+      const { queryByTestId } = render(
+        <AddressInput value={'invalidENS.eth'} onChange={jest.fn()} />
+      );
+
+      const addressAvatar = queryByTestId('avatar');
+
+      expect(addressAvatar).not.toBeInTheDocument();
+    });
+  });
+
   describe(`ENS behaviour`, () => {
     it(`should return an address value if ENSName is ENABLED and inputs an ENS name`, async () => {
       const mockOnChange = jest.fn();
@@ -92,69 +169,59 @@ describe(`AddressInput`, () => {
       expect(mockOnChange).toHaveBeenCalledWith(MOCK_ENS_NAME);
     });
 
-    it(`should populate the input with the ENS name if the value is an address`, async () => {});
-    it(`should populate the input with the address if the value is an address and ENSName is DISABLED`, async () => {});
+    it(`should populate the input with the ENS name if the value is an address`, async () => {
+      const { findByTestId } = render(
+        <AddressInput value={ZERO_ADDRESS} onChange={jest.fn()} />
+      );
+
+      const addressField = await findByTestId('address input');
+
+      expect(addressField).toHaveDisplayValue(MOCK_ENS_NAME);
+    });
+
+    it(`should populate the input with the address if the value is an address and ENSName is DISABLED`, async () => {
+      const { findByTestId } = render(
+        <AddressInput
+          value={ZERO_ADDRESS}
+          onChange={jest.fn()}
+          enableENSName={false}
+        />
+      );
+
+      const addressField = await findByTestId('address input');
+
+      expect(addressField).toHaveDisplayValue(ZERO_ADDRESS);
+    });
   });
 
-  it(`should clear the input if the X is clicked`, async () => {
-    const { findByTestId } = render(
-      <AddressInput value={MOCK_ADDRESS} onChange={jest.fn()} />
-    );
+  describe(`Snapshots`, () => {
+    it(`should match snapshot with address`, async () => {
+      const { container } = render(
+        <AddressInput value={MOCK_ADDRESS} onChange={jest.fn()} />
+      );
+      expect(container).toMatchSnapshot();
+    });
 
-    const addressField = await findByTestId('address input');
-    const clearAddressButton = await findByTestId('clear address');
-    fireEvent.click(clearAddressButton);
+    it(`should match snapshot with ENS disabled`, async () => {
+      const { container } = render(
+        <AddressInput
+          value={MOCK_ADDRESS}
+          onChange={jest.fn()}
+          enableENSName={false}
+        />
+      );
+      expect(container).toMatchSnapshot();
+    });
 
-    expect(addressField).toHaveDisplayValue('');
+    it(`should match snapshot isInvalid`, async () => {
+      const { container } = render(
+        <AddressInput
+          value={'0xinvalidAddress'}
+          onChange={jest.fn()}
+          isInvalid={true}
+        />
+      );
+      expect(container).toMatchSnapshot();
+    });
   });
-
-  it(`should be disabled`, async () => {
-    const { findByTestId } = render(
-      <AddressInput value={MOCK_ADDRESS} onChange={jest.fn()} disabled={true} />
-    );
-
-    const addressField = await findByTestId('address input');
-
-    expect(addressField).toBeDisabled();
-  });
-
-  it(`should show the avatar if the address is valid`, async () => {
-    const { findByTestId } = render(
-      <AddressInput
-        value={MOCK_ADDRESS}
-        onChange={jest.fn()}
-        enableENSName={false}
-      />
-    );
-
-    const addressAvatar = await findByTestId('avatar');
-
-    expect(addressAvatar).toBeInTheDocument();
-  });
-
-  it(`should show the avatar if the ENS name is valid`, async () => {
-    const { findByTestId } = render(
-      <AddressInput value={MOCK_ADDRESS} onChange={jest.fn()} />
-    );
-
-    const addressAvatar = await findByTestId('avatar');
-
-    expect(addressAvatar).toBeInTheDocument();
-  });
-
-  it(`shouldn't show the avatar if the address is invalid`, async () => {
-    const { queryByTestId } = render(
-      <AddressInput
-        value={'0xinvalidAddress'}
-        onChange={jest.fn()}
-        enableENSName={false}
-      />
-    );
-
-    const addressAvatar = queryByTestId('avatar');
-
-    expect(addressAvatar).not.toBeInTheDocument();
-  });
-
-  it(`shouldn't show the avatar if the ENS name is invalid`, async () => {});
 });
