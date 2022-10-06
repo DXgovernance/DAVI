da6d3a6aa8be14daef7358c361238dce645ad246
Kenny <kenny.chung@live.se>
2022-10-06 03:28:38 +0200

    feat: added function signature to permission

--- src/components/ActionsBuilder/ActionBuilder.tsx
+++ src/components/ActionsBuilder/ActionBuilder.tsx
@@ -10,6 +10,7 @@ import { useTranslation } from 'react-i18next';
 import { useGetETHPermission } from 'Modules/Guilds/Hooks/useETHPermissions';
 import { SupportedAction } from './types';
 import { useTypedParams } from 'Modules/Guilds/Hooks/useTypedParams';
+import Web3 from "web3";
 
 interface ActionsBuilderProps {
   options: Option[];
@@ -22,80 +23,78 @@ export const ActionsBuilder: React.FC<ActionsBuilderProps> = ({
   options,
   onChange,
 }) => {
+  const web3 = new Web3()
   const { t } = useTranslation();
   const { guildId: guildAddress } = useTypedParams();
   const [isEditable, setIsEditable] = useState(editable);
   const [showAddModal, setShowAddModal] = useState(false);
   const [editableOption, setEditableOption] = useState(null);
+  const [permissions, setPermissions] = useState([]);
 
+  const ethPermission = useGetETHPermission({
+    guildAddress,
+    from: permissions[0]?.from,
+    to: permissions[0]?.to,
+    funcSign: permissions[0]?.funcSign,
+  })
+  console.log({ ethPermission})
   const onEdit = () => setIsEditable(true);
 
   const onSave = useCallback(() => {
     const decodedActions = options[0]?.decodedActions;
     const permissionParameters = decodedActions.map(action => {
+      const functionInputs = action?.decodedCall?.function?.inputs?.map(input => input.type)
+      const functionName = action?.decodedCall?.function?.name
+      const functionSignature = web3.eth.abi.encodeFunctionSignature(`${functionName}(${functionInputs.join(',')})`)
       switch (action.decodedCall.callType) {
         case SupportedAction.ERC20_TRANSFER:
           return {
             from: action.decodedCall.from,
             to: action.decodedCall.to,
-            funcSign: action.decodedCall.args.functionSignature,
+            funcSign: functionSignature,
           };
         case SupportedAction.ENS_UPDATE_CONTENT:
           return {
             from: action.decodedCall.from,
             to: action.decodedCall.to,
-            funcSign: action.decodedCall.args.functionSignature,
+            funcSign: functionSignature,
           };
         case SupportedAction.GENERIC_CALL:
           return {
             from: action.decodedCall.from,
             to: action.decodedCall.to,
-            funcSign: action.decodedCall.args.functionSignature,
+            funcSign: functionSignature,
           };
         case SupportedAction.SET_PERMISSIONS:
           return {
             from: action.decodedCall.from,
             to: action.decodedCall.to,
-            funcSign: action.decodedCall.args.functionSignature,
+            funcSign: functionSignature,
           };
         case SupportedAction.REP_MINT:
           return {
             from: action.decodedCall.from,
             to: action.decodedCall.to,
-            funcSign: action.decodedCall.args.functionSignature,
+            funcSign: functionSignature,
           };
           case SupportedAction.NATIVE_TRANSFER:
-            return true
+            return {
+            from: action.decodedCall.from,
+            to: action.decodedCall.to,
+            funcSign: "",
+            };
         default:
           throw new Error('Unsupported action type');
       }
+    
     });
 
-
-    const permissions = permissionParameters.map(params => {
-      if(params === true) {
-        return true
-      }
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const ethPermission = useGetETHPermission({
-        guildAddress,
-        from: params.from,
-        to: params.to,
-        funcSign: params.funcSign,
-      })
-      if(ethPermission.data === '0') {
-        return false
-      } else {
-        return true
-      }
-    }
-    );
-    console.log({permissions})
-
+    Promise.all(permissionParameters).then(() => setPermissions(permissions))
+    
     const encodedOptions = bulkEncodeCallsFromOptions(options);
     onChange(encodedOptions);
     setIsEditable(false);
-  }, [onChange, options, guildAddress]);
+  }, [options, permissions, onChange, web3.eth.abi]);
 
   return (
     <SidebarCard
