cea24483d055b119597961aca140b020ac4f971d
Tomas Pulenta <tpulenta@gmail.com>
2022-10-31 06:21:44 -0500

    updated richContracts.json

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,2 +1,2 @@
-export const RICH_CONTRACT_DATA_REGISTRY = 'QmcouC1PhvSY4CeYZsWui9Anz2gVjD2D35PuDrqfc7Sx8w';
+export const RICH_CONTRACT_DATA_REGISTRY = 'QmXYFb8Q4qKgMPc7czqEgy8u9jY4kFHro6UiycbYwKtSRq';
 export const SITE_ID = 'NYKJUYYQ'
