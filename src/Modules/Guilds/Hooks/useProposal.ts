f03fbaf36f34b3bb60ae14582f787c7c45f75d26
Dino <crescimbenidino@gmail.com>
2022-11-14 16:00:13 -0300

    feat(useProposal): removed watch:true and added event listeners

--- src/Modules/Guilds/Hooks/useProposal.ts
+++ src/Modules/Guilds/Hooks/useProposal.ts
@@ -1,7 +1,7 @@
 import { unix } from 'moment';
 import BaseERC20GuildContract from 'contracts/BaseERC20Guild.json';
 import { Proposal, ContractState, InitialProposal } from 'types/types.guilds.d';
-import { useContractRead } from 'wagmi';
+import { useContractEvent, useContractRead } from 'wagmi';
 import { BigNumber } from 'ethers';
 
 export const formatterMiddleware = (data: InitialProposal): Proposal => {
@@ -27,15 +27,36 @@ export const formatterMiddleware = (data: InitialProposal): Proposal => {
   return clone as Proposal;
 };
 
+const getProposalIdFromEvent = event => event[0];
+
 const useProposal = (guildId: string, proposalId: string) => {
-  const { data, ...rest } = useContractRead({
+  const { data, refetch, ...rest } = useContractRead({
     addressOrName: guildId,
     contractInterface: BaseERC20GuildContract.abi,
     functionName: 'getProposal',
     args: proposalId,
-    watch: true,
   });
   const proposalData = data as unknown as InitialProposal;
+
+  useContractEvent({
+    addressOrName: guildId,
+    contractInterface: BaseERC20GuildContract.abi,
+    eventName: 'ProposalStateChanged',
+    listener(event) {
+      const eventProposalId = getProposalIdFromEvent(event);
+      if (proposalId === eventProposalId) refetch();
+    },
+  });
+
+  useContractEvent({
+    addressOrName: guildId,
+    contractInterface: BaseERC20GuildContract.abi,
+    eventName: 'VoteAdded',
+    listener(event) {
+      const eventProposalId = getProposalIdFromEvent(event);
+      if (proposalId === eventProposalId) refetch();
+    },
+  });
 
   return {
     data: data
