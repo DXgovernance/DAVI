469500bbbb4aeb269b1e881c927c85af55e51a0d
Dino <crescimbenidino@gmail.com>
2022-12-28 14:20:41 -0300

    feat(useTotalLocked): implemented hook with different logic based on governance type

--- src/stores/modules/SnapshotERC20Guild/fetchers/useTotalLocked.ts
+++ src/stores/modules/SnapshotERC20Guild/fetchers/useTotalLocked.ts
@@ -1,20 +1,20 @@
-import { BigNumber } from 'ethers';
-import { useMatch } from 'react-router';
-import { useHookStoreProvider } from 'stores';
 import useTotalLockedAt from 'Modules/Guilds/Hooks/useTotalLockedAt';
-import { useContractRead } from 'wagmi';
+import useGuildImplementationType from 'Modules/Guilds/Hooks/useGuildImplementationType';
+import { useContractEvent, useContractRead } from 'wagmi';
+import { BigNumber } from 'ethers';
 import { BaseERC20Guild } from 'contracts/ts-files/BaseERC20Guild';
+import { useHookStoreProvider } from 'stores';
+import { useMatch } from 'react-router-dom';
 
 const useTotalLocked = (guildAddress: string, snapshotId?: string) => {
-  // TODO: make useTypedMatch
-  // TODO: remove snapshotId optional param, since it's fetched the same way inside the hook
-  // Hooks call
+  console.log('ERC20 guild implementation');
   const {
     hooks: {
       fetchers: { useSnapshotId },
     },
+    isLoading,
   } = useHookStoreProvider();
-
+  // Hooks call
   const urlParams = useMatch('/:chainName/:guildId/proposal/:proposalId/*');
   const proposalId = urlParams?.params.proposalId as `0x${string}`; // TODO: remove type coercion
 
@@ -25,16 +25,43 @@ const useTotalLocked = (guildAddress: string, snapshotId?: string) => {
 
   const SNAPSHOT_ID = snapshotId ?? _snapshotId?.toString() ?? null;
 
+  const { loaded } = useGuildImplementationType(guildAddress);
+
+  const { data: totalLockedResponse, refetch } = useContractRead({
+    address: !isLoading ? guildAddress : null,
+    abi: BaseERC20Guild.abi,
+    functionName: 'getTotalLocked',
+  });
+
+  useContractEvent({
+    address: guildAddress,
+    abi: BaseERC20Guild.abi,
+    eventName: 'TokensLocked',
+    listener() {
+      refetch();
+    },
+  });
+
+  useContractEvent({
+    address: guildAddress,
+    abi: BaseERC20Guild.abi,
+    eventName: 'TokensWithdrawn',
+    listener() {
+      refetch();
+    },
+  });
+
   const { data: totalLockedAtProposalSnapshotResponse } = useTotalLockedAt({
     contractAddress: guildAddress,
     snapshotId: SNAPSHOT_ID,
   });
 
-  const { data: totalLockedResponse } = useContractRead({
-    address: guildAddress,
-    abi: BaseERC20Guild.abi,
-    functionName: 'getTotalLocked',
-  });
+  // Return response based on implementation type
+  if (!loaded) {
+    return {
+      data: undefined,
+    };
+  }
 
   return SNAPSHOT_ID
     ? {
--- src/stores/modules/SnapshotRepGuild/fetchers/useTotalLocked.ts
+++ src/stores/modules/SnapshotRepGuild/fetchers/useTotalLocked.ts
@@ -1,41 +1,69 @@
+import useGuildToken from 'Modules/Guilds/Hooks/useGuildToken';
+import useTotalSupplyAt from 'Modules/Guilds/Hooks/useTotalSupplyAt';
+import useGuildImplementationType from 'Modules/Guilds/Hooks/useGuildImplementationType';
+import { useContractEvent, useContractRead } from 'wagmi';
 import { BigNumber } from 'ethers';
-import { useMatch } from 'react-router';
-import { useHookStoreProvider } from 'stores';
-import useTotalLockedAt from 'Modules/Guilds/Hooks/useTotalLockedAt';
-import { useContractRead } from 'wagmi';
 import { BaseERC20Guild } from 'contracts/ts-files/BaseERC20Guild';
+import { useHookStoreProvider } from 'stores';
+import { useMatch } from 'react-router-dom';
 
 const useTotalLocked = (guildAddress: string, snapshotId?: string) => {
-  // TODO: make useTypedMatch
-  // TODO: remove snapshotId optional param, since it's fetched the same way inside the hook
-  // Hooks call
+  console.log('rep guild implementation');
   const {
     hooks: {
       fetchers: { useSnapshotId },
     },
   } = useHookStoreProvider();
-
+  // Hooks call
   const urlParams = useMatch('/:chainName/:guildId/proposal/:proposalId/*');
   const proposalId = urlParams?.params.proposalId as `0x${string}`; // TODO: remove type coercion
 
+  const { data: guildTokenAddress } = useGuildToken(guildAddress);
+
   const { data: _snapshotId } = useSnapshotId({
-    contractAddress: guildAddress,
+    contractAddress: guildTokenAddress,
     proposalId,
   });
 
   const SNAPSHOT_ID = snapshotId ?? _snapshotId?.toString() ?? null;
 
-  const { data: totalSupplyAtSnapshotResponse } = useTotalLockedAt({
-    contractAddress: guildAddress,
-    snapshotId: SNAPSHOT_ID,
-  });
+  const { loaded } = useGuildImplementationType(guildAddress);
 
-  const { data: totalLockedResponse } = useContractRead({
-    address: guildAddress,
+  const { data: totalLockedResponse, refetch } = useContractRead({
+    address: loaded ? guildAddress : null,
     abi: BaseERC20Guild.abi,
     functionName: 'getTotalLocked',
   });
 
+  useContractEvent({
+    address: guildAddress,
+    abi: BaseERC20Guild.abi,
+    eventName: 'TokensLocked',
+    listener() {
+      refetch();
+    },
+  });
+
+  useContractEvent({
+    address: guildAddress,
+    abi: BaseERC20Guild.abi,
+    eventName: 'TokensWithdrawn',
+    listener() {
+      refetch();
+    },
+  });
+
+  const { data: totalSupplyAtSnapshotResponse } = useTotalSupplyAt({
+    contractAddress: guildTokenAddress,
+    snapshotId: SNAPSHOT_ID,
+  });
+
+  // Return response based on implementation type
+  if (!loaded) {
+    return {
+      data: undefined,
+    };
+  }
   return SNAPSHOT_ID
     ? {
         data: totalSupplyAtSnapshotResponse
