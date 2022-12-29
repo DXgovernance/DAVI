9ab36845553616e5f01f6fb755f71b3bfd999106
rossneilson <rossneilson99@gmail.com>
2022-12-29 14:47:46 +0000

    style: Removing comments

--- src/components/DiscussionCard/DiscussionCard.tsx
+++ src/components/DiscussionCard/DiscussionCard.tsx
@@ -2,8 +2,6 @@ import { Flex } from 'components/primitives/Layout';
 import { UnstyledLink } from 'components/primitives/Links';
 import { shortenAddress } from 'utils';
 import { CardTitle, MainWrapper, FooterElement } from './DiscussionCard.styled';
-// import { BiHeart } from 'react-icons/bi';
-// import { MdReply } from 'react-icons/md';
 import useENSAvatar from 'hooks/Guilds/ens/useENSAvatar';
 import { DiscussionCardProps } from './types';
 import { useTypedParams } from 'Modules/Guilds/Hooks/useTypedParams';
@@ -27,12 +25,6 @@ export const DiscussionCard: React.FC<DiscussionCardProps> = ({
           <FooterElement>
             {ensName || shortenAddress(creatorAddress)}
           </FooterElement>
-          {/* <FooterElement>
-            <BiHeart size="20px" /> {discussion?.count_likes}
-          </FooterElement>
-          <FooterElement>
-            <MdReply size="20px" /> {discussion?.count_replies}
-          </FooterElement> */}
         </Flex>
       </MainWrapper>
     </UnstyledLink>
