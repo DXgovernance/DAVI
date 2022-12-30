c8630aa2acb1bdd900d637078204b8a822277e02
Dino <crescimbenidino@gmail.com>
2022-12-30 09:17:20 -0300

    fix: reduced the frequency of mock data suource switching logic

--- src/stores/modules/SnapshotRepGuild/checkDataSourceAvailability.ts
+++ src/stores/modules/SnapshotRepGuild/checkDataSourceAvailability.ts
@@ -1,6 +1,15 @@
+/*
+  This is a test function that switches the default data source
+  with a 50% chance, just to check how a (very unreliable)
+  data source interaction will look like.
+  You should modify the params to make it less annoying.
+  In the future, this will contain the actual logic to switch
+  between data sources.
+*/
+
 export const checkDataSourceAvailability = () => {
-  const num = Math.floor(Math.random() * 2);
-  if (num === 1) {
+  const num = Math.floor(Math.random() * 4);
+  if (num !== 0) {
     console.log(`${num}: now using default data source`);
     return true;
   } else {
