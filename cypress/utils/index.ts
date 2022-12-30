2668b129a9cd44d4fdf83487129eba678f578582
Dino <crescimbenidino@gmail.com>
2022-12-30 14:40:58 -0300

    test: changed clickAnywhereToClose

--- cypress/utils/index.ts
+++ cypress/utils/index.ts
@@ -1,6 +1,6 @@
 export * from './constants';
 
 export const clickAnywhereToClose = (nodeOrSelector: string = 'body') => {
-  return cy.get(nodeOrSelector).first().click(0, 0).wait(1000);
+  return cy.get(nodeOrSelector).first().click(1, 1, { force: true }).wait(1000);
 };
 
